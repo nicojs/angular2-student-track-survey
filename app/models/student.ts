91004a266f9998fcb797edeb04a7c38dc700328a
nicojs <jansennico@gmail.com>
2016-04-01 13:26:50 +0200

    Done Lab 2 TypeScript

--- app/models/student.ts
+++ app/models/student.ts
@@ -1,14 +1,25 @@
 // COMPLETE THE STUDENT CLASS
 // expose firstname, lastname and school properties
-export class Student{
-	// your code here...
-	
-
+export class Student {
+    constructor(public firstname: string, public lastname: string, public school: string) {
+    }
 }
 
 // COMPLETE THE STUDENTTRACK CLASS
 // expose name property, expose addStudentToTrack and getStudents methods
 export class StudentTrack {
-	// your code here...
-  
+
+    private students: Student[];
+
+    constructor(public name: string){
+        this.students = [];
+    }
+
+    public addStudentToTrack(student: Student) {
+        this.students.push(student);
+    }
+    
+    public getStudents(){
+        return this.students;
+    }
 }
