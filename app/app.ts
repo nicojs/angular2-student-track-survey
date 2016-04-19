b95846716cf0af076defd47b0be610258a662b79
Nico (N.C.) Jansen <nico.jansen@ing.nl>
2016-04-19 17:20:21 +0200

    Done lab 7: Routing

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 import { Component, provide } from 'angular2/core';
 import { bootstrap } from 'angular2/platform/browser';
 import {RouteConfig,  RouterLink, RouterOutlet, ROUTER_DIRECTIVES, ROUTER_PROVIDERS,
-        LocationStrategy, HashLocationStrategy} from 'angular2/router'; 
+        LocationStrategy, HashLocationStrategy, PathLocationStrategy} from 'angular2/router'; 
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {Student, StudentTrack} from './models/student';
 import {StudentDetails} from './components/studentdetails';
@@ -20,22 +20,24 @@ import 'rxjs/Rx';
 	   <h1 class="dark-primary-color text-primary-color">Student track {{studentTrack.name}} (<span [textContent]="studentTrack.getStudents().length"></span> attendees)</h1>
 	    <student-details 
 			[student]="student" 
-			[isSelected]="currentStudent === student"
+			[routerLink]="['./Student', {id: student.id }]"
 			*ngFor="#student of studentTrack.getStudents()"
             (deleted)="removeStudent(studentTrack, student)" 
 			(selected)="setSelected(student)"> 
 		</student-details>
 	 </div>
+     <div class="router-outlet"><router-outlet></router-outlet> </div>
 	`,
     styles: [`
 	 .student { padding:15px; }
 	 .studentTrack { border:1px solid black;margin:5px;padding:0px; }
 	 .studentTrack h1 { margin:0px;padding:15px;}
+     .router-outlet { margin: 5px; }
 	`],
-    directives: [StudentDetails, StudentTrackForm, /* ?? code ?? */]
+    directives: [StudentDetails, StudentTrackForm, RouterLink, RouterOutlet]
 })
 @RouteConfig([
-	// ?? code ??
+	{ path:'/student/:id', as:'Student', component:StudentCard}
 ])
 export class SurveyApplication {
     public studentTracks: StudentTrack[];
@@ -58,4 +60,4 @@ export class SurveyApplication {
 
 // bootstrap our application..
 bootstrap(SurveyApplication, [StudentTrackService, StudentService, HTTP_PROVIDERS,
-ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]); 
+ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: PathLocationStrategy})]); 
